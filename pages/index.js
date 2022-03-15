c7f0f0ffeccb11e5cb27e1661b8607137b4af8ff
Hunter Caron <huntercaron@me.com>
2022-03-15 17:45:05 +0100

    Updating deps

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from "next/head"
-import { Tip } from "../components/Tip"
-import { Container } from "../components/Container"
+import Head from "next/head";
+import { Tip } from "../components/Tip";
+import { Container } from "../components/Container";
 
 // A Smart Component from Framer
-import Toggle from "https://framer.com/m/Toggle-B5iT.js@X94mtocAj5XithxLESme"
+import Toggle from "https://framer.com/m/Toggle-B5iT.js@52zFaz7rN7Bt3pjtYxWH";
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Framer</title>
-                <meta name="description" content="Modules" />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Framer</title>
+        <meta name="description" content="Modules" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
 
-            <Container>
-                <Toggle />
-                <Tip />
-            </Container>
-        </>
-    )
+      <Container>
+        <Toggle />
+        <Tip />
+      </Container>
+    </>
+  );
 }
