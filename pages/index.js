ec407e7f5e3731b06c8a13bac6709e9d5e11d40b
Sonny Lazuardi <sonny.lazuardi@grabtaxi.com>
2022-08-06 16:20:55 +0800

    Add framer import

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,20 @@
 import Head from "next/head";
-import { Tip } from "../components/Tip";
 import { Container } from "../components/Container";
 
 // A Smart Component from Framer
-import Toggle from "https://framer.com/m/Toggle-B5iT.js@52zFaz7rN7Bt3pjtYxWH";
+import Player from "https://framer.com/m/Player-9OhH.js@joAOLHvsRNgtUp2LnLfN";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Framer</title>
+        <title>Flipbook Mobile</title>
         <meta name="description" content="Modules" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <Container>
-        <Toggle />
-        <Tip />
+        <Player />
       </Container>
     </>
   );
