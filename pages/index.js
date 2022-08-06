34e07fe2eb41f30e6aef4158d7e81eb323007cd8
Sonny Lazuardi <sonny.lazuardi@grabtaxi.com>
2022-08-06 18:08:35 +0800

    Update component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Container } from "../components/Container";
 
 // A Smart Component from Framer
-import Player from "https://framer.com/m/Player-9OhH.js@joAOLHvsRNgtUp2LnLfN";
+import Player from "https://framer.com/m/Player-9OhH.js@EPRfz3Q0gZMwLH0snwey";
 
 export default function Home() {
   return (
