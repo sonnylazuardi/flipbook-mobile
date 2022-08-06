c1786fbb181c9904fb0d051d917c888c4f123db2
Sonny Lazuardi <sonny.lazuardi@grabtaxi.com>
2022-08-06 18:29:49 +0800

    Update lock sc

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Container } from "../components/Container";
 
 // A Smart Component from Framer
-import PlayerWeb from "https://framer.com/m/Player-9OhH.js@EPRfz3Q0gZMwLH0snwey";
+import Player from "https://framer.com/m/Player-9OhH.js@c6FijxheZrXfJDcI3ekC";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
 
       <Container>
-        <PlayerWeb />
+        <Player />
       </Container>
     </>
   );
